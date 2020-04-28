aac2f213656592bc023d9804f1497b2c3cf10634
Zeeshan-2k1 <ashraf2k1zeeshan@gmail.com>
2020-04-28 12:02:35 +0530

    Password encryption using Hashing Algorithm.

--- README.md
+++
@@ -1,5 +0,0 @@
-# Auth-Project
-Authentication project 
-
-Commit - "Password encryption with Enviroment Var."
-Its is the first commit form my side which includes a basic level encryption of password using mongoose-encryption package.
--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5")
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -18,8 +18,6 @@ const userSchema = new mongoose.Schema ({
   email: String,
   password: String
 });
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = mongoose.model("User", userSchema);
 
@@ -39,7 +37,7 @@ app.get("/register", function(req,res){
 app.post("/register", function(req,res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err){
@@ -53,7 +51,7 @@ app.post("/register", function(req,res){
 
 app.post("/login", function(req,res){
   const userName = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: userName}, function(err, foundUser){
     if(err) {
